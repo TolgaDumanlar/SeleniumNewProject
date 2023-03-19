5e6c4fdecf8ccb774e9c56d78d2f24c82e34c2cf
Tolga Dumanlar <dumanlartolga@gmail.com>
2023-03-19 10:02:01 +0300

    Initial commit

---
+++ src/test/java/Ilkclass.java
@@ -0,0 +1,20 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Ilkclass {
+    public static void main(String[] args) {
+
+        ChromeOptions ops = new ChromeOptions();
+
+        ops.addArguments("--remote-allow-origins=*");
+
+
+
+        WebDriver driver = new ChromeDriver(ops) ;
+
+
+
+        driver.get("https://www.techproeducation.com");
+    }
+}
