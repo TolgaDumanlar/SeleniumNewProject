bebdb94c7be1ae02e8252bd673129d4187e280c1
Tolga Dumanlar <dumanlartolga@gmail.com>
2023-03-27 13:34:04 +0300

    v1

--- src/test/java/day04exercise/E02_cssSelector.java
+++ src/test/java/day04exercise/E02_cssSelector.java
@@ -2,12 +2,13 @@ package day04exercise;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.time.Duration;
 
 public class E02_cssSelector {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         WebDriver driver=new EdgeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
@@ -17,9 +18,25 @@ public class E02_cssSelector {
 
         //Add Element butonuna basin
 
-        driver.findElement(By.cssSelector("div="));
+        WebElement addElement=driver.findElement(By.cssSelector("button[onclick='addElement()']"));
+        addElement.click();
+        Thread.sleep(2000);
+
+
         //Delete butonu’nun gorunur oldugunu test edin
+        WebElement deleteElement=driver.findElement(By.cssSelector("button[onclick='addElement()']"));
+        System.out.println(deleteElement.isDisplayed());
+
+
         //Delete tusuna basin
+        Thread.sleep(3000);
+        deleteElement.click();
+
         //“Add/Remove Elements” yazisinin gorunur oldugunu test edin
+        WebElement addRemoveElement=driver.findElement(By.xpath("//h3"));
+        System.out.println(addRemoveElement.isDisplayed());
+
+        driver.close();
+
     }
 }
